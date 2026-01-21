2b46f28c4bdc873aebad46e3bd993ae1b1fb3831
williand019 <alzakelvar@gmail.com>
2026-01-21 07:58:30 -0700

    asdf

---
+++ SheTicsOnMyTacToe/nut1.py
@@ -0,0 +1,47 @@
+class Node:
+    def __init__(self, x):
+        self.data = x
+        self.links = []
+
+class Tree:
+    def __init__(self):
+        self.root = Node(0)
+
+    def grow(self):
+        for i in range(9):
+            self.root.links.append(Node(i))
+
+            for j in range(9):
+                self.root.links[i].links.append(Node((i, j)))
+
+    def loop(self):
+        for i in self.root.links:
+            if i.links:
+                print(i.data)
+                for j in i.links:
+                    print(j.data)
+
+result = None
+
+def traverse(root):
+     if root:
+#           print(root.data)
+        current = root
+        for i in current.links:
+            if i:
+                if i.data == (3,4):
+                    return i
+                else:
+                    temp = traverse(i)
+                    if temp:
+                        print()
+                        return temp
+
+        return None
+
+
+if __name__ == '__main__':
+    tree = Tree()
+    tree.grow()
+    print(traverse(tree.root).data)
+    #tree.loop()
