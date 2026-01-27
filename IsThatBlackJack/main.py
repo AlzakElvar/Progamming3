355bf528da775490f30c8eefea98b11401fa4469
SchoolBoy <alzakelvar@gmail.com>
2026-01-27 08:54:35 -0700

    new

--- IsThatBlackJack/main.py
+++ IsThatBlackJack/main.py
@@ -1,8 +1,10 @@
-import random as r
+import random
 
 class GameManager:
     def __init__(self):
         self.score = 0
+        self.money = 5000
+        self.bet = 0
         self.hand = []
 
     def calc_score(self):
@@ -25,7 +27,12 @@ class GameManager:
                 self.score += 11 #11 if goated
 
     def draw(self, deck):
-        self.hand.append(deck.pop(r.randint(0, len(deck))))
+        self.hand.append(deck.pop( random.randint(0, len(deck))) ) # Pop a random entry from deck and add to hand.
+
+    def print_hand(self):
+        for i in self.hand:
+            print(str(i), end= ", ")
+        print("")
 
 
 class Card:
@@ -60,7 +67,6 @@ class Card:
         return f"{rank} of {suit}"
 
 
-
 class CustomerService:
     def __init__(self):
         pass
@@ -77,3 +83,9 @@ if __name__ == "__main__":
     for s in range(4):
         for r in range(14):
             deck.append(Card(s, r))
+
+    GM.draw(deck)
+    GM.draw(deck)
+    GM.calc_score()
+    GM.print_hand()
+    print(GM.score)
