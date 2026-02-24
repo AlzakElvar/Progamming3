bd49e9badb489f57446520df82fa43e937fb61f3
SchoolBoy <alzakelvar@gmail.com>
2026-02-24 08:59:36 -0700

    Fish

--- IShipIt/main.java
+++ IShipIt/main.java
@@ -1,64 +1,77 @@
-class Test{
+import java.util.Scanner;
+
+class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World\n");
+        Scanner scanner = new Scanner(System.in);
         Grid board = new Grid();
         board.printGrid();
+
+        System.out.print("Please plink: ");
+        String plonk = scanner.nextLine();
+        board.plink(plonk);
+
+        board.printGrid();
+
+        scanner.close();
     }
 }
 
 class Grid {
     
-    int[][] grid = new int[10][10];
+    char[][] grid = new char[10][10];
+
+    public Grid(){
+       
+        for (int i = 0; i < grid.length; i++){
+            for (int j = 0; j < grid[i].length; j++){
+                grid[i][j] = '0';
+            }
+        }
+    }
     
     public void printGrid() {
         
-        for (int[] row : grid) {
-            for (int col : row) {
-        
-                if (col == 0) {                     //0 means base state
-                    System.out.print(col + "  ");
-                } 
-                else if (col == 1) {                //1 means miss
-                    System.out.print("0  ");
-                } 
-                else if (col == 2){                 //2 means hit
-                    System.out.print("X  ");
-                } 
-                else if (col == 3){
-                    System.out.print("█  ");        //3 means ship (unhit)
-                }
-                else if (col == 4){
-                    System.out.print("⍁ ");        //4 means ship (hit)
-                }
+        for (char[] row : grid) {
+            for (char col : row) {
+
+                System.out.print(col + "  ");
+
+                //     System.out.print("0  ")
+                //     System.out.print("O  ");
+                //     System.out.print("X  ");
+                //     System.out.print("█  ");        //3 means ship (unhit)
+                //     System.out.print("[ ");        //4 means ship (hit)
                 
             }
         System.out.print("\n");
         }
     }
 
-    public int collumn(char arg) {
+    public int getCollumn(char arg) {
         char[] key = {
-            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'j'
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h','i', 'j'
         };
         
-        int answer = 0;
+        int answer = -1;
         
         for (int i = 0; i < key.length; i++ ) {
-            answer = i;
+            if (arg == key[i]) {
+                answer = i;
+            }
         }
         
-        return key[0];
+        return answer;
     }
 
+
+    // Will take a String of XA: where X is any letter between A - J and A is any number between 1 - 10
+    
     public void plink(String args) {
         char a = args.charAt(0);
-        int x = 0;
+        int b = Integer.parseInt( Character.toString(args.charAt(1)) );
         
-        if (a == 'a') {
-            x = 0;
-        }
+        int c = getCollumn(a);
         
-
-        System.out.print(x);
+        grid[b][c] = 'O';
     }
 }
