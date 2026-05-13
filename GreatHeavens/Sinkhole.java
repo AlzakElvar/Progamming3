f81a58e689b02847355ec997888277f12b0facdf
SchoolBoy <alzakelvar@gmail.com>
2026-05-13 08:40:06 -0600

    pelase help me(ssage)

--- GreatHeavens/Sinkhole.java
+++ GreatHeavens/Sinkhole.java
@@ -1,12 +1,32 @@
+
+import java.util.Scanner;
+
 public class Sinkhole {
     public static void main(String[] args)
     {
+        Scanner scan = new Scanner(System.in);
+
         //Prompt user for input:
         //      Type of graph -> A
+        System.out.println("Please enter the kind of graph you want:");
+        System.out.println("""
+                LineGraph
+                ScatterPlot
+                PieChart
+                Histogram
+                """);
+        String A = scan.nextLine();
+
         //          case statement to switch:
         //              XY or otherwise
+
         //Repeat until interrupt:
         //  Prompt user for input:
+        String B = "ello";
+        while (!B.equals(""))
+        {
+            B = scan.nextLine();
+        }
         //          Manual or File
         //              Prompt for file - autoassign labels
         //              Prompt with lables - autograb data
