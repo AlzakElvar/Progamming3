5d9b1b5058e20b6e2ee3ddff9fd2668f0d2ee850
SchoolBoy <alzakelvar@gmail.com>
2026-05-13 08:15:50 -0600

    boink

---
+++ GreatHeavens/Sinkhole.java
@@ -0,0 +1,35 @@
+public class Sinkhole {
+    public static void main(String[] args)
+    {
+        //Prompt user for input:
+        //      Type of graph -> A
+        //          case statement to switch:
+        //              XY or otherwise
+        //Repeat until interrupt:
+        //  Prompt user for input:
+        //          Manual or File
+        //              Prompt for file - autoassign labels
+        //              Prompt with lables - autograb data
+        //          Prompt for data
+
+    }
+}
+
+class Constructor
+{
+    String a;
+    public Constructor(String A)
+    {
+        a = A;
+    }
+
+    public void build()
+    {
+        System.out.print(a);
+        //Switch between:
+        //      Line Graph
+        //      Scatter Plot
+        //      PieChart
+        //      Histograms
+    }
+}
